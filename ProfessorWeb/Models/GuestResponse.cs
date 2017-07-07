dbacc3d64669af25513591053e45565e99f266e0
gerasimenko <gerasimenko.serge@icloud.com>
2017-07-07 17:40:28 +0300

    Добавьте файлы проекта.

---
+++ ProfessorWeb/Models/GuestResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProfessorWeb.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
